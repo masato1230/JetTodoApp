bd365f3cb8119a94b6f2ce7c7ef3e55b2ba7429a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-03 17:05:00 +0900

    Add FAB and EditDialog UI

--- app/src/main/java/com/example/jettodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jettodoapp/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.example.jettodoapp.components.EditDialog
 import com.example.jettodoapp.ui.theme.JetTodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,8 +26,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    val isShowDialog = remember { mutableStateOf(false) }
+    if (isShowDialog.value) {
+        EditDialog(isShowDialog)
+    }
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { isShowDialog.value = true }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
     }
 }
