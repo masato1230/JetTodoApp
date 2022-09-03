2ae23172bd9f9cdceafd296bde02b978aed3f69e
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-03 17:43:14 +0900

    Connect TextFields and ViewModel

--- app/src/main/java/com/example/jettodoapp/components/EditDialog.kt
+++ app/src/main/java/com/example/jettodoapp/components/EditDialog.kt
@@ -9,18 +9,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettodoapp.MainViewModel
 
 @Composable
 fun EditDialog(isShowDialog: MutableState<Boolean>) {
+    val viewModel = hiltViewModel<MainViewModel>()
+
     AlertDialog(
         onDismissRequest = { isShowDialog.value = false },
         title = { Text(text = "タスク新規作成") },
         text = {
             Column {
                 Text(text = "タイトル")
-                TextField(value = "", onValueChange = { /* Todo */ })
+                TextField(
+                    value = viewModel.title,
+                    onValueChange = { viewModel.title = it },
+                )
                 Text(text = "詳細")
-                TextField(value = "", onValueChange = { /* Todo */ })
+                TextField(
+                    value = viewModel.description,
+                    onValueChange = { viewModel.description = it },
+                )
             }
         },
         buttons = {
